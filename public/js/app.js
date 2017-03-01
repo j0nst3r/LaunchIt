f22eac060c88f3767b83d6bb858a5a6eb60ae767
Jonathan Chen <jonathan.chen.89@gmail.com>
2017-02-28 16:21:16 -0800

    registration shell and controller edits

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,1 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+angular.module('LaunchIt', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService', 'RegistrationCtrl']);

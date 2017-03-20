0881ac222ea9868ac1aee3dafb133915a3cbe790
Jonathan Chen <jonathan.chen.89@gmail.com>
2017-03-20 00:27:30 -0700

    home page added card and minor ui fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
-angular.module('LaunchIt', ['ngRoute','ngFileUpload','ngImgCrop','ui.bootstrap','appRoutes',
+angular.module('LaunchIt', ['ngRoute','ngFileUpload','ngImgCrop','ui.bootstrap','appRoutes', 'ngMaterial', 'ngAnimate', 'ngAria',
 							'LaunchesCtrl', 'LoginCtrl', 'MainCtrl', 'NotificationCtrl', 'ProfileCtrl', 'registration', 'accountpage', 'launchBoard', 'createLaunch',
 							'DataService']);

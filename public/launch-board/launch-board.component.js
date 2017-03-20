65aa6b67b0319a25b836ec8d32da7b0b90c090ea
kkorolyov <kirillk753@gmail.com>
2017-03-19 23:03:10 -0700

    Fix hardcoded launch image

--- public/launch-board/launch-board.component.js
+++ public/launch-board/launch-board.component.js
@@ -12,7 +12,7 @@ angular.
 		controller: ["$location", "dataService", function (location, dataService) {
 
 			this.launches = [
-				new Launch("LaunchIt", "A web application which shares the ideas of startup companies.", "https://lh6.googleusercontent.com/VqsfOtouEj7tSCMd9bodm722nzBCCSpTtPKyrQKjO2FykHILjjipkp01vvYaspTZHLiZyTaN1V_CzIc=w1920-h945"),
+				new Launch("LaunchIt", "A web application which shares the ideas of startup companies.", "https://s-media-cache-ak0.pinimg.com/236x/bd/6e/5c/bd6e5ce4130322f588640258fca7b03b.jpg"),
 				new Launch("Pinterest", "Find and save recipes, parenting hacks, style inspiration and other ideas to try.", "https://s-media-cache-ak0.pinimg.com/avatars/pinterest_1475538227_280.jpg"),
 				new Launch("Kickstarter", "The world's largest funding platform for creative projects. A home for film, music, art, theater, games, comics, design, photography, and more.", "https://www.kickstarter.com/download/kickstarter-logo-k-color.png")
 			]

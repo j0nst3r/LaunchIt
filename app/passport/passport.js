65bf379a3e06177571204edd7504f00c7d81f6d1
joshsylvia <josh.sylvia@sjsu.edu>
2017-04-22 22:54:03 -0700

    Login Social Media

--- app/passport/passport.js
+++ app/passport/passport.js
@@ -36,98 +36,136 @@ module.exports = function(app, passport){
     passport.use(new FacebookStrategy({
         clientID: '823444951139581',
         clientSecret: '174e3e5d33d503a5ac77d23161eea78e',
-        callbackURL: "http://localhost:8080/auth/facebook/callback",
+        callbackURL: "http://ec2-54-215-234-96.us-west-1.compute.amazonaws.com/auth/facebook/callback",
         profileFields: ['id', 'displayName', 'email']
     },
-    function(accessToken, refreshToken, profile, done) {
-        console.log( profile._json.email);  //email
-        var name = profile._json.name.split(" ");
-        console.log( name[0] ); // FirstName
-        console.log( name[1] ); // LastName
-        var passIn = "Password!";
+    function(accessToken, refreshToken, socialMedia, done) {
+        user.findOne({ 'socialId': socialMedia.id }, function(err, curUser) {
+			console.log(JSON.stringify(profile));
+            if (err) {
+                return done(err);
+            }
+            if (!curUser) {
+                curUser = new user({
+                socialId: socialMedia.id
+                });
+				
+                curUser.save(function(err) {
+                    if (err) {console.log(err);
+					}else{
+					console.log("finding user and creating profile");
+					user.findOne({'socialId': socialMedia.id }, function(err , result){
+                       console.log(result._id); 
+					var newProfile = new profile({
+						_id : result._id
+					});
+					newProfile.save(function(err){
+						if (err) console.log(err);
+					});
+					
+					});
+					}
+				});
+				return done(null, curUser);
+				
+            } else {
+				console.log("EXISTING USER...")
+                //found user. Return
+                return done(null, curUser);
+            }
+        });
     }));
 
     app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
-    app.get('/auth/facebook/callback', passport.authenticate('facebook', {  successRedirect: '/', failureRedirect: '/login', scope:['email'] }));
-
+    app.get('/auth/facebook/callback',  passport.authenticate('facebook', { failureRedirect: '/login' }),
+		function(req, res){
+			var url = "/login/";
+			var userId = req.user._id;
+			console.log(req.user._id);
+			url = url.concat(userId);
+			console.log(url);
+            res.redirect(url);
+    });
 
 //==================== Twitter ===========================================
 
     passport.use(new TwitterStrategy({
         consumerKey: 'ZqlQympcucnIMB4P0aVpZnGJK',
         consumerSecret: 'CICrKoL3KQaT9P9p5QzsUvSnjO2Oea1xVzZc2n6zezvNS3khGK',
-        callbackURL: "http://localhost:8080/auth/twitter/callback",
+        callbackURL: "http://ec2-54-215-234-96.us-west-1.compute.amazonaws.com/auth/twitter/callback",
         userProfileURL: "https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true"
     },
-    function(token, tokenSecret, profile, done) {
-
-       console.log(profile.emails[0].value); // email
-       console.log(profile.displayName);   // DisplayName "Captain Teemo"
- 
-       
-      done(null,profile);
-    }
-    ));
-
-    app.get('/auth/twitter', passport.authenticate('twitter'));
-    app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/', failureRedirect: '/login' }));
-
-// ==================== LinkedIn ===========================================
-
-    passport.use(new LinkedInStrategy({
-        clientID: '86pgcisvxgos0e',
-        clientSecret: 'IrlzgEBf9mZeJFwR',
-        callbackURL: "http://localhost:8080/auth/linkedin/callback",
-        scope: ['r_emailaddress', 'r_basicprofile'],
-    }, 
-    function(accessToken, refreshToken, profile, done) {
-        //console.log('linkedin profile', profile);
-        process.nextTick(function () {
-            
-            console.log(profile.emails[0].value);
-            console.log(profile.name.givenName);
-            console.log(profile.name.familyName);
-            
-            return done(null, profile);
-        });
-    }));
-
-    app.get('/auth/linkedin/callback', passport.authenticate('linkedin', { successRedirect: '/', failureRedirect: '/login' }));
-    app.get('/auth/linkedin',  passport.authenticate('linkedin', { state: 'SOME STATE'  }),  function(req, res){
-    });
-
-  
-//==================== Google ===========================================
-
-    passport.use(new GoogleStrategy({
-        clientID: '358732923710-ito5k4gg0s8qadi5416npkhesa6g9aj7.apps.googleusercontent.com',
-        clientSecret: 'ar5cLIUNgZtJqeXm-y4CHKlV',
-        callbackURL: "http://localhost:8080/auth/google/callback"
-    },
-	function(accessToken, refreshToken, socialMedia, done) {
-        //check user table for anyone with a socialId of profile.id
-        user.findOne({
-            'socialId': socialMedia.id 
-        }, function(err, curUser) {
-			console.log(JSON.stringify(socialMedia));
+    function(token, tokenSecret, socialMedia, done) {
+        //check user table for anyone with a profile.id
+        user.findOne({ 'socialId': socialMedia.id }, function(err, curUser) {
+			console.log(JSON.stringify(profile));
             if (err) {
                 return done(err);
             }
-            //No user was found... so create a new user with values from(all the profile. stuff)
             if (!curUser) {
                 curUser = new user({
-                    //name: profile.displayName,
-                    //email: profile.emails[0].value,
-                    //username: profile.username,
-                    //provider: 'facebook',
-                    //now in the future searching on User.findOne({'facebook.id': profile.id } will match because of this next line
                     socialId: socialMedia.id
                 });
 				
                 curUser.save(function(err) {
                     if (err) {console.log(err);
 					}else{
-					console.log("finding user and createing profile");
+					console.log("finding user and creating profile");
+					user.findOne({'socialId': socialMedia.id }, function(err , result){
+					var newProfile = new profile({
+						_id : result._id
+					});
+					newProfile.save(function(err){
+						if (err) console.log(err);
+					});
+					
+                    });
+					}
+				});
+				return done(null, curUser);
+				
+            } else {
+				console.log("EXISTING USER...")
+                return done(null, curUser);
+            }
+        });
+    }));
+
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+    app.get('/auth/twitter/callback',  passport.authenticate('twitter', { failureRedirect: '/login' }),
+		function(req, res){
+			var url = "/login/";
+			var userId = req.user._id;
+			console.log(req.user._id);
+			url = url.concat(userId);
+			console.log(url);
+            res.redirect(url);
+    });
+
+// ==================== LinkedIn ===========================================
+
+    passport.use(new LinkedInStrategy({
+        clientID: '86pgcisvxgos0e',
+        clientSecret: 'IrlzgEBf9mZeJFwR',
+        callbackURL: "http://ec2-54-215-234-96.us-west-1.compute.amazonaws.com/auth/linkedin/callback",
+        scope: ['r_emailaddress', 'r_basicprofile'],
+    }, 
+    function(accessToken, refreshToken, socialMedia, done) {
+
+        user.findOne({ 'socialId': socialMedia.id }, function(err, curUser) {
+			console.log(JSON.stringify(profile));
+            if (err) {
+                return done(err);
+            }
+            if (!curUser) {
+                curUser = new user({
+                    socialId: socialMedia.id
+                });
+				
+                curUser.save(function(err) {
+                    if (err) {console.log(err);
+					} else {
+					console.log("finding user and creating profile");
 					user.findOne({'socialId': socialMedia.id }, function(err , result){
 					var newProfile = new profile({
 						_id : result._id
@@ -144,6 +182,63 @@ module.exports = function(app, passport){
             } else {
 				console.log("EXISTING USER...")
                 //found user. Return
+                return done(null, curUser);
+            }
+        });
+    }));
+
+
+    app.get('/auth/linkedin',  passport.authenticate('linkedin', { state: 'SOME STATE'  }),  function(req, res){ });
+    app.get('/auth/linkedin/callback',  passport.authenticate('linkedin', { failureRedirect: '/login' }),
+		function(req, res){
+			var url = "/login/";
+			var userId = req.user._id;
+			console.log(req.user._id);
+			url = url.concat(userId);
+			console.log(url);
+            res.redirect(url);
+    });
+  
+//==================== Google ===========================================
+
+    passport.use(new GoogleStrategy({
+        clientID: '358732923710-ito5k4gg0s8qadi5416npkhesa6g9aj7.apps.googleusercontent.com',
+        clientSecret: 'ar5cLIUNgZtJqeXm-y4CHKlV',
+        callbackURL: "http://ec2-54-215-234-96.us-west-1.compute.amazonaws.com/auth/google/callback"
+    },
+	function(accessToken, refreshToken, socialMedia, done) {
+        user.findOne({
+            'socialId': socialMedia.id 
+        }, function(err, curUser) {
+			console.log(JSON.stringify(socialMedia));
+            if (err) {
+                return done(err);
+            }
+            if (!curUser) {
+                curUser = new user({
+                    socialId: socialMedia.id
+                });
+				
+                curUser.save(function(err) {
+                    if (err) {console.log(err);
+					}else{
+					console.log("finding user and creating profile");
+					user.findOne({'socialId': socialMedia.id }, function(err , result){
+                        console.log(result._id); 
+					var newProfile = new profile({
+						_id : result._id
+					});
+					newProfile.save(function(err){
+						if (err) console.log(err);
+					});
+					
+					});
+					}
+				});
+				return done(null, curUser);
+				
+            } else {
+				console.log("EXISTING USER...")
                 return done(null, curUser);
             }
         });

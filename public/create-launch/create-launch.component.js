3f2b17ab255281645b2bf01715a09f940e252210
IamKenshin <ryantruong1993@gmail.com>
2017-04-25 20:14:34 -0700

    Preview section for image uploads.

--- public/create-launch/create-launch.component.js
+++ public/create-launch/create-launch.component.js
@@ -6,7 +6,44 @@ angular.
 		templateUrl: "create-launch/create-launch.template.html",
 		controller: function($scope, $rootScope, $location, dataService) {
 			$scope.imgList = []
+			var fileUpload = document.getElementById('uploads')
+			fileUpload.onchange = function() {
+				var preview = document.getElementById('preview')
+				if(fileUpload.files.length > 5) {
+					alert("Please select no more than 5 images")
+					$('uploads').val("")
+					while(preview.hasChildNodes()) {
+						preview.removeChild(preview.lastChild)
+					}
+				}
+				else {
+					while(preview.hasChildNodes()) {
+						preview.removeChild(preview.lastChild)
+					}
+					for(let i = 0; i < fileUpload.files.length; i++) {
+					var file = fileUpload.files[i]
+					var reader = new FileReader()
+					reader.onload = function(e) {
+						var img = document.createElement('img')
+						img.height = "100"
+						img.width = "100"
+						img.src = e.target.result
+						preview.appendChild(img)
+						if(preview.childNodes.length > 5){
+							alert("Please select no more than 5 images!")
+							while(preview.hasChildNodes()) {
+								preview.removeChild(preview.lastChild)
+							}
+						}
+					}
+						reader.readAsDataURL(file)
+					}
+				}
+				
+			}
+			
 			$scope.test = function(fieldName, fileList) {
+
 				for(let i = 0; i < fileList.length; i++) {
 					$scope.$apply(function() {
 						$scope.imgList.push(fileList[i])

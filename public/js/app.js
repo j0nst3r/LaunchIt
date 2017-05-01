f0a88a807cee372771f4a700b2ff097a3d008ca1
Mark <mark.bragg90@gmail.com>
2017-05-01 14:04:42 -0700

    componentation of home, extracting rerendering code

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
 angular.module('LaunchIt', ['ngRoute','ngFileUpload','ngImgCrop','ui.bootstrap','appRoutes', 'ngMaterial', 'ngAnimate', 'ngAria',
 							'home', 'LoginCtrl', 'MainCtrl', 'NotificationCtrl', 'ProfileCtrl', 'registration', 'accountpage', 'launchBoard', 'createLaunch',
-							'DataService']);
+							'DataService', 'uiController']);

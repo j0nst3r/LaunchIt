17ac69c5159caca2a06ae5af57cd6a25976cfa3c
kkorolyov <kirillk753@gmail.com>
2017-05-01 15:26:04 -0700

    Add launch

---
+++ public/launch/launch.component.js
@@ -0,0 +1,33 @@
+"use strict"
+
+angular
+	.module('launch', ['ui.bootstrap'])
+	.component('launch', {
+		templateUrl: 'launch/launch.template.html',
+
+		bindings: {
+			resolve: '<',
+			close: '&',
+			dismiss: '&'
+		},
+		controller: ['dataService', function (dataService) {
+			this.$onInit = function () {
+				this.launch = angular.copy(this.resolve.launch)
+				this.edit = this.resolve.edit
+
+				this.title = function () { return (this.edit ? "Edit " : "") + this.launch.name }
+				
+			}
+
+			this.return = function (del) {
+				this.close({
+					$value: {
+						data: this.launch,
+						del: del
+					}
+				})
+			}
+			this.ok = function () { this.return() }
+			this.delete = function () { this.return(false) }
+		}]
+	})

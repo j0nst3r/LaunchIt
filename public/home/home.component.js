6a695d0ae403845dd1c82ce28fd3aa5687962cd6
Jonathan Chen(d) <jonathan.chen.89@gmail.com>
2017-05-07 02:00:06 -0700

    wip

--- public/home/home.component.js
+++ public/home/home.component.js
@@ -25,6 +25,11 @@ angular
                 });
             });
             
+            $(window).scroll(function() {
+            if($(window).scrollTop() == $(document).height() - $(window).height()) {
+                // ajax call get data from server and append to the div
+                }
+            });
 
 
             this.launches = []

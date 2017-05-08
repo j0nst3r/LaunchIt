f9beb914e874ce31090bea694f322d0cbef0a1ff
IamKenshin <ryantruong1993@gmail.com>
2017-05-08 13:01:58 -0700

    update property.js

--- config/property.js
+++ config/property.js
@@ -1,4 +1,4 @@
 module.exports = {
 	dbUrl : 'mongodb://dev:dev@ds017736.mlab.com:17736/f17cmpe165',
-	apiUrl : 'http://localhost:8080/'
+	apiUrl : 'http://ec2-54-215-234-96.us-west-1.compute.amazonaws.com/'
 }
